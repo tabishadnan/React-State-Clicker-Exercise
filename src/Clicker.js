5a49073f8565773a30336aceaeda873842eabd99
tabishadnan <tabishadnan000@gmail.com>
2020-09-25 23:20:42 -0700

    Initial commit

---
+++ src/Clicker.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Clicker extends Component {
+
+    state = {
+        num : 1
+    }
+
+    handleClicker = () => {
+        const randNum = Math.floor(Math.random() * 10) + 1;
+
+        this.setState({
+            num : randNum
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Number : {this.state.num}</h3>
+                {this.state.num === 7 ? "YOU WIN!" :  <button onClick={this.handleClicker}>Click Me</button>}
+            </div>
+        );
+    }
+}
+
+export default Clicker;
